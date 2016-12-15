79f2eccafc974262cce5d5716fdc182ffe2e44f8
Jen Evans <jen@ppmroadmap.com>
2016-12-15 12:51:53 -0500

    transition tweaks

--- app/_assets/js/app.js
+++ app/_assets/js/app.js
@@ -7,31 +7,31 @@ $(document).ready(function(){
 
 $( ".interaction-1" ).hover(
   function() {
-    $( ".interaction-1" ).addClass( "bg-white" );
+    $( ".interaction-1" ).addClass( "table-fade" );
   }, function() {
-    $( ".interaction-1" ).removeClass( "bg-white" );
+    $( ".interaction-1" ).removeClass( "table-fade" );
   }
 );
 
 $( ".interaction-2" ).hover(
   function() {
-    $( ".interaction-2" ).addClass( "bg-white" );
+    $( ".interaction-2" ).addClass( "table-fade" );
   }, function() {
-    $( ".interaction-2" ).removeClass( "bg-white" );
+    $( ".interaction-2" ).removeClass( "table-fade" );
   }
 );
 
 $( ".interaction-3" ).hover(
   function() {
-    $( ".interaction-3" ).addClass( "bg-white" );
+    $( ".interaction-3" ).addClass( "table-fade" );
   }, function() {
-    $( ".interaction-3" ).removeClass( "bg-white" );
+    $( ".interaction-3" ).removeClass( "table-fade" );
   }
 );
 
 $( ".interaction-slide" ).hide();
 $( ".interaction-slide" ).reverse().each(function(i) {
-  $(this).delay(3000*i).slideIn();
+  $(this).delay(2500*i).fadeIn();
 });
 
 });
