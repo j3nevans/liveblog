343529955f67b7b8c6b5216018cb28dcb3d4dabc
Jen Evans <jen@ppmroadmap.com>
2016-12-15 12:14:47 -0500

    fade in js function for live demo

--- app/_assets/js/app.js
+++ app/_assets/js/app.js
@@ -1,29 +1,37 @@
+jQuery.fn.reverse = function() {
+    return this.pushStack(this.get().reverse(), arguments);
+};
+
 $(document).ready(function(){
 
 
-    $( ".interaction-1" ).hover(
-      function() {
-        $( ".interaction-1" ).addClass( "bg-white" );
-      }, function() {
-        $( ".interaction-1" ).removeClass( "bg-white" );
-      }
+$( ".interaction-1" ).hover(
+  function() {
+    $( ".interaction-1" ).addClass( "bg-white" );
+  }, function() {
+    $( ".interaction-1" ).removeClass( "bg-white" );
+  }
 );
 
-    $( ".interaction-2" ).hover(
-      function() {
-        $( ".interaction-2" ).addClass( "bg-white" );
-      }, function() {
-        $( ".interaction-2" ).removeClass( "bg-white" );
-      }
+$( ".interaction-2" ).hover(
+  function() {
+    $( ".interaction-2" ).addClass( "bg-white" );
+  }, function() {
+    $( ".interaction-2" ).removeClass( "bg-white" );
+  }
 );
 
-    $( ".interaction-3" ).hover(
-      function() {
-        $( ".interaction-3" ).addClass( "bg-white" );
-      }, function() {
-        $( ".interaction-3" ).removeClass( "bg-white" );
-      }
+$( ".interaction-3" ).hover(
+  function() {
+    $( ".interaction-3" ).addClass( "bg-white" );
+  }, function() {
+    $( ".interaction-3" ).removeClass( "bg-white" );
+  }
 );
 
+$( ".interaction-slide" ).hide();
+$( ".interaction-slide" ).reverse().each(function(i) {
+  $(this).delay(3000*i).slideIn();
+});
 
 });
