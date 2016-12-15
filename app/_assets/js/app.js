e50d69ce753ba7fb7065dfd432b368ad8f04af96
Jen Evans <jen@ppmroadmap.com>
2016-12-15 10:56:28 -0500

    jquery interaction applied to pricing table

---
+++ app/_assets/js/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+
+    $( ".interaction-1" ).hover(
+      function() {
+        $( ".interaction-1" ).addClass( "bg-white" );
+      }, function() {
+        $( ".interaction-1" ).removeClass( "bg-white" );
+      }
+);
+
+    $( ".interaction-2" ).hover(
+      function() {
+        $( ".interaction-2" ).addClass( "bg-white" );
+      }, function() {
+        $( ".interaction-2" ).removeClass( "bg-white" );
+      }
+);
+
+    $( ".interaction-3" ).hover(
+      function() {
+        $( ".interaction-3" ).addClass( "bg-white" );
+      }, function() {
+        $( ".interaction-3" ).removeClass( "bg-white" );
+      }
+);
+
+
+});
